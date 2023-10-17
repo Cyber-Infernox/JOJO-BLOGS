5dd8be80ba3cae2321fc7c224e80df2ff54394e8
Cyber_Infernox <sayonkar@gmail.com>
2023-10-17 23:21:05 +0530

    Client updates

--- Client/my-app/src/Pages/SinglePost.jsx
+++ Client/my-app/src/Pages/SinglePost.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Footer from "../Components/Footer";
 import Navbar from "../Components/Navbar";
 
@@ -5,7 +6,40 @@ const SinglePost = () => {
   return (
     <>
       <Navbar />
-      <div>SinglePost</div>
+      <div className="single">
+        <div className="content">
+          <img src="" alt="" />
+          <div className="user">
+            <img src="" alt="" />
+            <div className="info">
+              <span>John</span>
+              <p>Posted 2 days ago</p>
+            </div>
+            <div className="edit">
+              <Link to={`/write?edit=2`}>
+                <button>Edit</button>
+              </Link>
+              <button>Delete</button>
+            </div>
+          </div>
+          <h1>Lorem ipsum dolor sit amet</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
+            iure repudiandae aliquam mollitia voluptatibus. Vero distinctio
+            maxime delectus a soluta, saepe architecto atque illo exercitationem
+            nostrum doloremque debitis quaerat accusantium aliquid labore iusto
+            veniam suscipit consequatur, magni maiores repudiandae! Ducimus illo
+            incidunt quisquam impedit ipsam odit quis cupiditate illum, libero
+            corrupti, esse dolor pariatur numquam suscipit magni cumque nobis
+            nemo consequuntur aut exercitationem ratione! Rerum tenetur dolorem
+            nulla quisquam illo, sapiente odit eum, cupiditate assumenda dolores
+            fugiat deserunt esse nam aliquid at illum inventore optio obcaecati!
+            Ipsa eos consequatur repellat optio a explicabo mollitia illo
+            delectus? Accusantium, voluptatem? A, dicta!
+          </p>
+        </div>
+        <div className="menu">m</div>
+      </div>
       <Footer />
     </>
   );
